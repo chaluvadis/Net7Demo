819a8f8398199373fa8ce239c2b13fbd03014fdd
Surendra Chaluvadi <surendra.chaluvadi@gmail.com>
2023-01-28 11:33:55 +0400

    first commit

---
+++ AddProduct/AddProduct/Function.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AddProduct;
+
+public class Function
+{
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        return $"AddProduct - {request.Body.ToUpper()}";
+    }
+}
+
---
+++ DeleteProduct/DeleteProduct/Function.cs
@@ -0,0 +1,22 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace DeleteProduct;
+
+public class Function
+{
+
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        return $"DeleteProduct - {request.Body.ToUpper()}";
+    }
+}
